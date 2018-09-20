06609c6b14d0ab1e88a5571e97c965574458fc95
Emerson Franks <emersonf@microsoft.com>
2018-09-20 16:33:56 -0700

    bare bones
    initial commit

---
+++ Jupiter.Api/Controllers/DateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jupiter.Api.Controllers
+{
+    /// <summary>
+    ///     All things about dates.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatesController : ControllerBase
+    {
+        /// <summary>
+        ///     GET ~/date/day/number
+        /// </summary>
+        /// <returns></returns>
+        [Route("day/number")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(DateTime.Today.DayOfYear);
+        }
+    }
+}
---
+++ Jupiter.Api/Controllers/GuidController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jupiter.Api.Controllers
+{
+    /// <summary>
+    ///     All things GUID.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuidsController : ControllerBase
+    {
+        /// <summary>
+        ///     GET ~/guid
+        /// </summary>
+        /// <returns>A new GUID.</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(Guid.NewGuid());
+        }
+    }
+}
