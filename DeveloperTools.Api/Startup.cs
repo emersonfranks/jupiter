5c44bafd578a7c8a1f731578d1f93720bb3c4c8d
Emerson Franks <emersonf@microsoft.com>
2018-09-26 16:53:09 -0700

    I can haz creditz.

--- DeveloperTools.Api/Startup.cs
+++ DeveloperTools.Api/Startup.cs
@@ -21,7 +21,21 @@ namespace DeveloperTools.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "Developer Tools", Version = "v1" }); });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Developer Tools",
+                    Version = "v1",
+                    Description = "Just some tools that I seem to always need.",
+                    Contact = new Contact
+                    {
+                        Email = "franks.emerson@gmail.com",
+                        Name = "Emerson Franks",
+                        Url = "https://www.linkedin.com/in/emersonfranks/"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
