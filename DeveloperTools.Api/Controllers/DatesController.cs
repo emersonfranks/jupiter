4d63cbf8574058f62464c5d844e9f3f69d080522
Emerson Franks <emersonf@microsoft.com>
2018-10-12 11:31:18 -0700

    Add ability to supply a DateTime

--- DeveloperTools.Api/Controllers/DatesController.cs
+++ DeveloperTools.Api/Controllers/DatesController.cs
@@ -22,6 +22,18 @@ namespace DeveloperTools.Api.Controllers
         }
 
         /// <summary>
+        ///     GET ~/date/day/number/{datetime}
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> the caller would like the day number of.</param>
+        /// <returns>The day number.</returns>
+        [Route("Day/Number/{dateTime}")]
+        [HttpGet]
+        public IActionResult Get(DateTime dateTime)
+        {
+            return this.Ok(dateTime.DayOfYear);
+        }
+
+        /// <summary>
         ///     GET ~/date/time/utcnow
         /// </summary>
         /// <returns>The current UTC date time.</returns>
